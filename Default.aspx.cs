d5fc292d30563a3265d32d73d561b68b652e08ab
Nour Bache <nour.bache@etu.cyu.fr>
2021-03-18 16:19:43 +0100

    string connection

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,7 +12,22 @@ namespace Projetbdd
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            string connectionString;
+            SqlConnection cnn;
+
+            connectionString = @"Server=localhost\SQLEXPRESS;Database=mydb;Trusted_Connection=True;";
+
+            cnn = new SqlConnection(connectionString);
+
+            cnn.Open();
+            Response.Write("Connection Made");
+            cnn.Close(); 
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
 
         }
+
     }
 }
